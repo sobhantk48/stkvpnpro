307931789781ada2590ca748edcb3dfe867b258f
tksobhan <tksobhan@users.noreply.github.com>
2026-06-26 22:13:55 +0330

    phase 7: ui architecture foundation

---
+++ lib/ui/dashboard/dashboard_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen
+    extends StatelessWidget {
+
+  const DashboardScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "STK VPN PRO",
+        ),
+      ),
+    );
+  }
+}
